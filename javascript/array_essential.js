11c65f5331b1ecc0c332c9e4a3d8aa706aa8584c
shubhangi kittur <shubhangikittur@gmail.com>
2022-03-31 16:25:57 +0530

    added array methods-pop,push,shift,unshift

---
+++ javascript/array_essential.js
@@ -0,0 +1,13 @@
+let arr=[100,200,300,400];
+//adding item to end os array
+arr.push(500);
+console.log(`added 500 to---> ${arr}`);
+//removing item from end of array
+arr.pop();
+console.log(`removed last element-->${arr}`);
+//add item to the beginning of array
+arr.unshift(99);
+console.log(`added 99 at the beginning of array---${arr}`);
+//remove an item from beginning of array
+arr.shift();
+console.log(`removed item from beginning-->${arr}`);
